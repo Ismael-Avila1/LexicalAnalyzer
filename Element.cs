55063dbf1e0b11ffec32257f08ba751dfce2b4a0
Ismael Avila <ismael.guada234015@gmail.com>
2022-09-16 00:14:12 -0500

    Creación de la clase Element, que es la que va a guardar el identificador, el token y el numero de cada uno de los elementos identificados,

---
+++ Element.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexicalAnalyzer
+{
+    internal class Element
+    {
+        string identifier;
+        string token;
+        int number;
+
+        public Element() { }
+        
+        public Element(string identifier, string token, int number) {
+            this.identifier = identifier;
+            this.token = token;
+            this.number = number;
+        }
+
+
+        public string Identifier
+        {
+            get { return identifier; }
+            set { identifier = value; }
+        }
+
+        public string Token
+        {
+            get { return token; }
+            set { token = value; }
+        }
+
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+    }
+}
