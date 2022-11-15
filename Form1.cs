036f16b8d1a4550c3b7b9cfd82281d587e1072a9
Ismael Avila <ismael.guada234015@gmail.com>
2022-11-14 22:08:51 -0600

    Autómata terminado. Checar el detalle que los operadores tienen que estar separados de los ids para que los reconozcca correctamente.

--- Form1.cs
+++ Form1.cs
@@ -36,26 +36,105 @@ namespace LexicalAnalyzer
                             lexeme += s[i];
                             token = "id";
                         }
-                        else if(s[i] == ';') {
+                        else if(char.IsDigit(s[i])) {
                             state = 2;
-                            lexeme += s[i];
-                            token = "Punto y coma";
-                        }
-                        else if(s[i] == '$') {
-                            state = 3;
-                            lexeme += s[i];
-                            token = "Pesos";
-                        }
-                        else if(s[i] == '=') {
-                            state = 4;
-                            lexeme += s[i];
-                            token = "Asignacion";
-                        }
-                        else if(Char.IsDigit(s[i])) {
-                            state = 6;
                             lexeme += s[i];
                             token = "Numero";
                         }
+                        else if(s[i] == ';') {
+                            elements.Add(new Element(s[i].ToString(), "Punto y coma", 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
+                        else if(s[i] == ',') {
+                            elements.Add(new Element(s[i].ToString(), "Coma", 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
+                        else if(s[i] == '(') {
+                            elements.Add(new Element(s[i].ToString(), "Parentesis Izq", 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
+                        else if(s[i] == ')') {
+                            elements.Add(new Element(s[i].ToString(), "Parentesis Der", 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
+                        else if(s[i] == '{') {
+                            elements.Add(new Element(s[i].ToString(), "Llave Izq", 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
+                        else if(s[i] == '}') {
+                            elements.Add(new Element(s[i].ToString(), "Llave Der", 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
+                        else if(s[i] == '$') {
+                            elements.Add(new Element(s[i].ToString(), "Pesos", 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
+                        else if(s[i] == '!') {
+                            state = 11;
+                            lexeme += s[i];
+                            token = "OpRelacional";
+                        }
+                        else if(s[i] == '>') {
+                            state = 13;
+                            lexeme += s[i];
+                            token = "OpRelacional";
+                        }
+                        else if(s[i] == '<') {
+                            state = 15;
+                            lexeme += s[i];
+                            token = "OpRelacional";
+                        }
+                        else if(s[i] == '=') {
+                            state = 17;
+                            lexeme += s[i];
+                            token = "Asignacion";
+                        }
+                        else if(s[i] == '|') {
+                            state = 19;
+                            lexeme += s[i];
+                            token = "OpLogico";
+                        }
+                        else if(s[i] == '&') {
+                            state = 21;
+                            lexeme += s[i];
+                            token = "OpLogico";
+                        }
+                        else if(s[i] == '+' || s[i] == '-') {
+                            elements.Add(new Element(s[i].ToString(), "OpSuma", 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
+                        else if(s[i] == '*' || s[i] == '/') {
+                            elements.Add(new Element(s[i].ToString(), "OpMultiplicacion", 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
+
                         break;
 
 
@@ -65,75 +144,145 @@ namespace LexicalAnalyzer
                             lexeme += s[i];
                             token = "id";
                         }
-                        else
-                            state = 20;
+                        else {
+                            elements.Add(new Element(lexeme, token, 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
                         break;
 
 
                     case 2:
-                        state = 20;
-                        break;
-
-
-                    case 3:
-                        state = 20;
-                        break;
-
-
-                    case 4:
-                        if(s[i] == '=') {
-                            state = 5;
-                            lexeme += s[i];
-                            token = "opRelacional";
-                        }
-                        else 
-                            state = 20;
-                        break;
-
-
-                    case 5:
-                        state = 20;
-                        break;
-
-
-                    case 6:
                         if(Char.IsDigit(s[i])) {
-                            state = 6;
+                            state = 2;
                             lexeme += s[i];
                             token = "Numero";
                         }
                         else if(s[i] == '.') {
-                            state = 7;
+                            state = 3;
                             lexeme += s[i];
                             token = "Numero";
                         }
-                        else
-                            state = 20;
+                        else {
+                            elements.Add(new Element(lexeme, token, 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
                         break;
 
 
-                    case 7:
+                    case 3:
                         if(Char.IsDigit(s[i])) {
-                            state = 7;
+                            state = 3;
                             lexeme += s[i];
                             token = "Numero";
                         }
-                        else 
-                            state = 20;
+                        else {
+                            elements.Add(new Element(lexeme, token, 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
                         break;
 
-                    case 20:
+
+                    case 11:
+                        if(s[i] == '=') {
+                            lexeme += s[i];
+                            token = "OpRelacional";
+
+                            elements.Add(new Element(lexeme, token, 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
+                        break;
+
+
+                    case 13:
+                        if(s[i] == '=') {
+                            lexeme += s[i];
+                            token = "OpRelacional";
+
+                            elements.Add(new Element(lexeme, token, 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
+                        break;
+
+
+                    case 15:
+                        if(s[i] == '=') {
+                            lexeme += s[i];
+                            token = "OpRelacional";
+
+                            elements.Add(new Element(lexeme, token, 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
+                        break;
+
+
+                    case 17:
+                        if(s[i] == '=') {
+                            lexeme += s[i];
+                            token = "OpRelacional";
+
+                            elements.Add(new Element(lexeme, token, 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
+                        else {
+                            elements.Add(new Element(lexeme, token, 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
+                        break;
+
+
+                    case 19:
+                        if(s[i] == '|') {
+                            lexeme += s[i];
+                            token = "OpLogico";
+
+                            elements.Add(new Element(lexeme, token, 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
+                        break;
+
+
+                    case 21:
+                        if(s[i] == '&') {
+                            lexeme += s[i];
+                            token = "OpLogico";
+
+                            elements.Add(new Element(lexeme, token, 0));
+
+                            state = 0;
+                            lexeme = "";
+                            token = "";
+                        }
                         break;
                 }
-                if(state == 20) {
-                    elements.Add(new Element(lexeme, token, 0));
-
-                    state = 0;
-                    lexeme = "";
-                    token = "";
-                }
+                
             }
-
             
         }
 
